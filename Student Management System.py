435a596340de0702c32c7873de337ad1e6a2f0ed
Apple <Apple@Mac.fpt>
2026-08-31 14:52:51 +0700

    Add new feature to Student Management System

--- Student Management System.py
+++ Student Management System.py
@@ -1,26 +1,52 @@
-student = [
-    {
-        "MSSV": 1,
-        "Name": "khanh",
-        "Age": 20,
-        "major": "IS"
+student = {
+    '001': {
+        'name': 'An',
+        'age': 19,
+        'gpa': 3.5
     },
-    {
-        "MSSV": 2,
-        "Name": "khoa",
-        "Age": 19,
-        "major": "IS"
+    '002': {
+        'name': 'Bình',
+        'age': 20,
+        'gpa': 3.7
     }
-]
-
-print(student)
+}
 
 def add_student():
-    mssv = input("mssv:")
-    name = inp
+    mssv = input("mssv: ")
+    if mssv in student:
+        print("already exists")
+        return 1
+    else:
+        student[mssv] = {
+            "name": input("name: "),
+            "age": int(input("age: ")),
+            "gpa": int(input("gpa: "))
+        }
 
-print("u want to append new student Y/N")
+# if add_student():
+#     print(student)
 
-if input() == 'Y':
-    while True:
+def find_student():
+    mssv = input("dien mssv: ")
+    if mssv in student:
+        print("da tim thay")
+        print(f"thong tin sv: {student[mssv]}")
+    else:
+        print("khong thay")
         
+# find_student()
+
+def remove_student():
+    mssv = input("dien mssv: ")
+    if mssv in student:
+        del student[mssv]
+        print("xoa thanh cong")
+        return 1
+    else:
+        print("khong tim thay")
+
+# if remove_student():
+#     print(student)
+
+
+
